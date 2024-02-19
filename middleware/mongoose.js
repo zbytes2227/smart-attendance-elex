41003a4ea9ca10211dcb239955231c06bc089345
zbytes2227 <zbytes0@gmail.com>
2024-02-19 13:14:18 +0530

    first commit

---
+++ middleware/mongoose.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const connectDb = (handler) => async (req, res) => {
+  if (mongoose.connections[0].readyState) {
+    return handler(req, res);
+  }
+  await mongoose.connect("mongodb+srv://zbytes1:OMrHtXsW9EdP5xSf@cluster0.45jhcsq.mongodb.net/alok-proj?retryWrites=true&w=majority");
+  return handler(req, res);
+};
+export default connectDb;
